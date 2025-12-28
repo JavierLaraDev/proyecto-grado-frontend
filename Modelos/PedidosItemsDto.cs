a491c907709a8d78d17b4b26569027829dcf348a
Javier Lara <javierlarasolis1414@gmail.com>
2025-12-28 15:47:27 -0400

    fix: prevent orders from being created with unauthenticated userId

--- Modelos/PedidosItemsDto.cs
+++ Modelos/PedidosItemsDto.cs
@@ -5,11 +5,7 @@ namespace PersonalizacionProyectoGradoWASM.Modelos
     public class PedidosItemsDto
     {
         public int Id { get; set; }
-        [Required]
-        public int AccesorioId { get; set; }
-        public Accesorio Accesorio { get; set; }
-        [Required]
-        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser al menos 1")]
         public int Cantidad { get; set; }
+        public Accesorio Accesorio { get; set; }
     }
 }
